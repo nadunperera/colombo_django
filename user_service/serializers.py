4f8d8b42a9ba8a423c8872301413c813e2442f1a
Nadun Perera <nadun.perera@me.com>
2018-09-15 09:33:34 +1000

    added new fields to model and the serializer

--- user_service/serializers.py
+++ user_service/serializers.py
@@ -6,7 +6,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = User
-        fields = ('url', 'id', 'first_name', 'last_name', 'age', 'email', 'role', 'lead_source', 'created_at', 'updated_at',)
+        fields = ('url', 'id', 'first_name', 'last_name', 'age', 'email', 'role', 'lead_source', 'mobile_number', 'office_number', 'address', 'address', 'city', 'post_code', 'purchasing_entity', 'can_not_contact', 'personal_income', 'partners_income', 'owned_properties', 'own_home', 'gst_registered', 'company_name', 'abn', 'australian_resident', 'firb_required', 'investment_needed', 'created_at', 'updated_at',)
 
 
 class RoleSerializer(serializers.HyperlinkedModelSerializer):
