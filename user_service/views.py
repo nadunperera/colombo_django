a3cfd0c5f4deec1c472125e1b6ae4b6b0b1ee7c9
Nadun Perera <nadun.perera@me.com>
2018-09-14 13:29:51 +1000

    lead_source protectederror handled.

--- user_service/views.py
+++ user_service/views.py
@@ -1,3 +1,4 @@
+from django.db.models import ProtectedError
 from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import User, Role, LeadSource
@@ -17,13 +18,19 @@ class RoleView(viewsets.ModelViewSet):
         queryset = Role.objects.filter(role_name=self.get_object()).first().users.first()
         if not queryset:
             self.perform_destroy(self.get_object())
-            return Response({'status': 'ok',
-                             'description': 'role deleted.'})
+            return Response({'status': 'ok', 'description': 'role deleted.'})
         else:
-            return Response({'status': 'error',
-                             'description': 'role is not empty.'})
+            return Response({'status': 'error', 'description': 'role is not empty.'})
 
 
 class LeadSourceView(viewsets.ModelViewSet):
     queryset = LeadSource.objects.all()
     serializer_class = LeadSourceSerializer
+
+    def destroy(self, request, pk=None):
+        try:
+            self.perform_destroy(self.get_object())
+        except ProtectedError:
+            return Response({'status': 'error', 'description': 'lead source is not empty.'})
+        else:
+            return Response({'status': 'ok', 'description': 'lead source deleted.'})
