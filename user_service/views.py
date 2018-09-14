21713f44066f2b419b656d8e5206e8315ed83d62
Nadun Perera <nadun.perera@me.com>
2018-09-14 13:15:49 +1000

    DELETE on role now checks whether empty or not.

--- user_service/views.py
+++ user_service/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import User, Role, LeadSource
 from .serializers import UserSerializer, RoleSerializer, LeadSourceSerializer
 
@@ -12,12 +13,15 @@ class RoleView(viewsets.ModelViewSet):
     queryset = Role.objects.all()
     serializer_class = RoleSerializer
 
-    def perform_destroy(self, instance):
-        queryset = Role.objects.filter(role_name=instance).first().users.first()
+    def destroy(self, request, pk=None):
+        queryset = Role.objects.filter(role_name=self.get_object()).first().users.first()
         if not queryset:
-            print('Ok you can delete!')
+            self.perform_destroy(self.get_object())
+            return Response({'status': 'ok',
+                             'description': 'role deleted.'})
         else:
-            print('You cannot delete for shit!')
+            return Response({'status': 'error',
+                             'description': 'role is not empty.'})
 
 
 class LeadSourceView(viewsets.ModelViewSet):
